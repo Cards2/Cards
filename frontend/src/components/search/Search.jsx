0d7c59ca5ea49d659ef48f919d2dd07692de9402
Melvin Gonzalez <57917756+Melvin-Code@users.noreply.github.com>
2020-03-11 17:56:27 -0400

    working

--- frontend/src/components/search/Search.jsx
+++ frontend/src/components/search/Search.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import action from "../../services/index";
 class Search extends Component {
   state = {
     allUsers: [],
@@ -7,6 +7,16 @@ class Search extends Component {
     userdata: this.props.generalstate.users
   };
 
+  profileCallFunc = () => {
+      
+    if (this.props.generalstate.users) {
+      return this.props.generalstate.users.data.allUsers.map((eachuser, i) => {
+        return action.profileCall(eachuser._id).then(res => {
+          console.log("fuckyeah");
+        });
+      });
+    }
+  };
   searchfunction = () => {
     if (this.props.generalstate.users) {
       return this.props.generalstate.users.data.allUsers.map((eachuser, i) => {
@@ -23,6 +33,9 @@ class Search extends Component {
   };
 
   render() {
+    {
+      this.profileCallFunc();
+    }
     return (
       <div>
         {this.searchfunction()}
