d99f9cbed47994243d1324de9ce5f9b88141ac05
RiskTolerance <isaac.druin@gmail.com>
2020-03-13 16:40:11 -0700

    card componant paralax :)

--- frontend/src/components/partials/Card.jsx
+++ frontend/src/components/partials/Card.jsx
@@ -1,38 +1,28 @@
 import React, { Component } from 'react'
+import Tilt from "react-tilt";
 
 export default class Card extends Component {
     render() {
         return (
-            // <div>
-            // <h3> Username: {eachuser.username} </h3>
-            // <h3> Title: {eachuser.title} </h3>
-            // <h3> About: </h3>
-            // <p> {eachuser.about} </p>
-            // <h3> Quote: </h3>
-            // <p> {eachuser.quote} </p>
-            // <h3> Art/Animation Skills: {eachuser.art}/10 </h3>
-            // <h3> Programming Skills: {eachuser.programming}/10 </h3>
-            // <h3> Design Skills: {eachuser.design}/10 </h3>
-            // <h3> Audio Skills: {eachuser.audio}/10 </h3>
-            // <h3> Writing Skills: {eachuser.writing}/10 </h3>
-
-            // <h2> Availability:</h2>
-            // {/* How are we displaying this information?? */}
-            // <h3> Monday:</h3>
-            // <h4> {eachuser.monday}</h4>
-            // <h3> Tuesday:</h3>
-            // <h4> {eachuser.tuesday}</h4>
-            // <h3> Wednesday:</h3>
-            // <h4> {eachuser.wednesday}</h4>
-            // <h3> Thursday:</h3>
-            // <h4> {eachuser.thursday}</h4>
-            // <h3> Friday:</h3>
-            // <h4> {eachuser.friday}</h4>
-            // <h3> Saturday:</h3>
-            // <h4> {eachuser.saturday}</h4>
-            // <h3> Sunday:</h3>
-            // <h4> {eachuser.sunday}</h4>
+            <Tilt
+            className='card Tilt'
+            options={{ max: 15 }}
+            style={{
+              height: 220,
+              width: 350,
+              transformStyle: "preserve-3d",
+              backgroundImage: `url(${"https://images.pexels.com/photos/1083822/pexels-photo-1083822.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"})`,
+              backgroundSize: "cover",
+              borderRadius: "6px"
+            }}
+          >
+            <div className='Tilt-inner innerCard'>
+              {" "}
+                <h1 className='cardName'>Isaac</h1>
+                <h3 className='cardTitle'>Graphic Designer</h3>{" "}
             </div>
+          </Tilt>
         )
     }
 }
+
