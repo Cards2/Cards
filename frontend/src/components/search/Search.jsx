05c65ab8ff45224175b14e3716602d1d1c508b30
David Torres <djjabbua@gmail.com>
2020-03-15 11:58:11 -0500

    sunday push

--- frontend/src/components/search/Search.jsx
+++ frontend/src/components/search/Search.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Tilt from "react-tilt";
 import action from "../../services/index";
 class Search extends Component {
   state = {
@@ -13,7 +14,7 @@ class Search extends Component {
       return this.props.generalstate.users.data.allUsers.map((eachuser, i) => {
         return (
           <>
-            <br></br>
+           <br></br>
             <h1> Search </h1>
 
           <h2> Card Profiles </h2>
@@ -53,10 +54,39 @@ class Search extends Component {
     }
   };
 
+cardTest = () => {
+  if (this.props.generalstate.users) {
+    return this.props.generalstate.users.data.allUsers.map((eachuser, i) => {
+      return (
+        <Tilt
+            className='card Tilt'
+            options={{ max: 15 }}
+            style={{
+              height: 220,
+              width: 350,
+              transformStyle: "preserve-3d",
+              backgroundImage: `url(${"https://images.pexels.com/photos/1083822/pexels-photo-1083822.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"})`,
+              backgroundSize: "cover",
+              borderRadius: "6px"
+            }}
+          >
+            <div className='Tilt-inner innerCard'>
+              {" "}
+                <h1 className='cardName'>{eachuser.username}</h1>
+                <h3 className='cardTitle'>{eachuser.title} </h3>{" "}
+            </div>
+          </Tilt>
+        );
+      });
+    }
+  };
+
 render() {
       return (
         <div>
-          {this.searchfunction()}
+          {/* {this.searchfunction()} */}
+          {this.cardTest()}
+
         </div>
       );
     }
