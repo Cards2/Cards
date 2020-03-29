59df6763004d3750bba9c7c882cda1b92db12ebb
RiskTolerance <unconfigured@null.spigotmc.org>
2020-03-28 20:11:51 -0400

    check for update

--- frontend/src/components/partials/Footer.jsx
+++ frontend/src/components/partials/Footer.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
-    render() {
-        return (
-            <div className='footer'>
-                
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className='footer'>
+				<h1>.</h1>
+			</div>
+		);
+	}
 }
 
 export default Footer;
